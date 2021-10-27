e8a2433c681a60158cf010259af92ad570bc42f2
pwshady <pwshady@gmail.com>
2021-10-27 14:47:27 +0300

    day-1

---
+++ HelloWorld/src/ru/aniks/hello/Program.java
@@ -0,0 +1,10 @@
+package ru.aniks.hello;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi");
+	}
+
+}
---
+++ Vars/src/ru/aniks/vars/Program.java
@@ -0,0 +1,13 @@
+package ru.aniks.vars;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		long x = ((long)Integer.MAX_VALUE) +1;
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(x);
+
+	}
+
+}
